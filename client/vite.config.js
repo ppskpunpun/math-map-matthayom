69667566a6eb8d8e0126fd9e0fe6e90730ab234d
Punpun <suracha2550@gmail.com>
2025-07-15 13:46:18 +0700

    Add dotenv to use VITE_BASE_PATH in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/math-map-matthayom',
+  base: process.env.VITE_BASE_PATH || '/math-map-matthayom',
   plugins: [
     react(),
     tailwindcss()
